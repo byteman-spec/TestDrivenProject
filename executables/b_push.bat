4d01da6750d8c3ca233aafe9575d13ac7439a667
byteman-spec <explodefromdelhi@gmail.com>
2024-04-21 01:14:25 +0530

    [byteman-spec]Updated b_push

--- executables/b_push.bat
+++ executables/b_push.bat
@@ -1,30 +1,53 @@
 @echo off
+setlocal EnableDelayedExpansion
 
-REM Set your repository owner, repository name, workflow name, and access token
-set GIT_USERNAME=%2
-set BRANCH_NAME=%1
-set WORKFLOW_NAME=build_pipeline
-set ACCESS_TOKEN=%3
 
-REM Check if the first parameter is -h
 if "%1"=="-h" (
     echo This is custom command used to push 
     echo code through the validation pipeline.
     echo Code is only pushed after the pipeline passes.
     echo Sample Usage	
-    echo b_push <branch_name> <GIT username> <PAT_token>	
+    echo b_push ^<branch_name^> ^<GIT_username^> ^<PAT_token^>	
     exit /b 1
 )
 
-REM Set your input parameter name
+set GIT_USERNAME=%2
+set BRANCH_NAME=%1
+set PROJECT_NAME=TestDrivenProject
+set WORKFLOW_NAME=94615098
+set ACCESS_TOKEN=%3
+
+
+if "%BRANCH_NAME%"=="" (
+    echo Branch name is missing.
+    exit /b 1
+)
+if "%GIT_USERNAME%"=="" (
+    echo GitHub username is missing.
+    exit /b 1
+)
+if "%ACCESS_TOKEN%"=="" (
+    echo Personal access token is missing.
+    exit /b 1
+)
+
 set INPUT_NAME=branch
 
-REM Accept input value as a parameter
 set INPUT_VALUE=%1Valid
 
-REM Trigger the workflow using cURL
+echo curl -X POST ^
+  -H "Authorization: token !ACCESS_TOKEN!" ^
+  -H "Accept: application/vnd.github.v3+json" ^
+	https://api.github.com/repos/%GIT_USERNAME%/%PROJECT_NAME%/actions/workflows/%WORKFLOW_NAME%/dispatches ^
+  -d "{\"ref\":\"main\", \"inputs\": {\"!INPUT_NAME!\": \"!INPUT_VALUE!\"}}"
+echo.
+
+
 curl -X POST ^
   -H "Authorization: token %ACCESS_TOKEN%" ^
   -H "Accept: application/vnd.github.v3+json" ^
-  https://api.github.com/repos/%GIT_USERNAME%/%BRANCH_NAME%/actions/workflows/%WORKFLOW_NAME%/dispatches ^
-  -d "{\"ref\":\"main\", \"inputs\": {\"%INPUT_NAME%\": \"%INPUT_VALUE%\"}}"
+  https://api.github.com/repos/%GIT_USERNAME%/%PROJECT_NAME%/actions/workflows/%WORKFLOW_NAME%/dispatches ^
+  -d "{\"ref\":\"main\", \"inputs\": {\"!INPUT_NAME!\": \"!INPUT_VALUE!\"}}"
+
+
+endlocal
