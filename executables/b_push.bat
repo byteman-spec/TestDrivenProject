4584c3eb9da0b8ae22b26e3d5f53afcb0af5cb01
byteman-spec <explodefromdelhi@gmail.com>
2024-04-21 02:19:51 +0530

    [byteman-spec]Finished b_push.bat

--- executables/b_push.bat
+++ executables/b_push.bat
@@ -11,37 +11,34 @@ if "%1"=="-h" (
     exit /b 1
 )
 
-set GIT_USERNAME=byteman-spec
 set BRANCH_NAME=%1
-set PROJECT_NAME=TestDrivenProject
-set WORKFLOW_NAME=94615098
 set ACCESS_TOKEN=%2
-
 
 if "%BRANCH_NAME%"=="" (
     echo Branch name is missing.
     exit /b 1
 )
-if "%GIT_USERNAME%"=="" (
-    echo GitHub username is missing.
-    exit /b 1
-)
+
 if "%ACCESS_TOKEN%"=="" (
     echo Personal access token is missing.
     exit /b 1
 )
 
+set GIT_USERNAME=byteman-spec
+set PROJECT_NAME=TestDrivenProject
+set WORKFLOW_NAME=94615098
+
 set INPUT_NAME_1=branchInput
 set INPUT_NAME_2=branchOutput
-
 set INPUT_VALUE_1=%1
 set INPUT_VALUE_2=%1Valid
+
 
 echo curl -X POST ^
   -H "Authorization: token !ACCESS_TOKEN!" ^
   -H "Accept: application/vnd.github.v3+json" ^
 	https://api.github.com/repos/%GIT_USERNAME%/%PROJECT_NAME%/actions/workflows/%WORKFLOW_NAME%/dispatches ^
-  -d "{\"ref\":\"main\", \"inputs\": {\"!INPUT_NAME!\": \"!INPUT_VALUE!\"}}"
+  -d "{\"ref\":\"main\", \"inputs\": {\"!INPUT_NAME_1!\": \"!INPUT_VALUE_1!\",\"!INPUT_NAME_2!\": \"!INPUT_VALUE_2!\"}}"
 echo.
 
 
